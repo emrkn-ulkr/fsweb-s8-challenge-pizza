8ad61a1bad26665f1b1fe0b8574c9b622a649f3f
r1semir <emirkanulker08@gmail.com>
2025-08-22 19:22:03 +0300

    Readme ve form dosyaları oluşturldu

---
+++ readme2.md
@@ -0,0 +1,3 @@
+1- Form componenti oluştur ve formun görselini bitir.
+2- Form için gereken mantık kodlarını bitir
+3- Form için gereken yüksek standart görsellerini uygula.
---
+++ src/components/Form.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Form() {
+    return (
+        <div>
+
+            <div className='header-from'>
+                <h1>Teknolojik yemekler</h1>
+            </div>
+
+            <div className='body-form'>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Form
