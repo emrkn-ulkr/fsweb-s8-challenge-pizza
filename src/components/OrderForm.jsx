fd72ed8e64b0e40dd10c898ba6d19cf450a151eb
r1semir <emirkanulker08@gmail.com>
2025-09-09 17:18:38 +0300

    OrderForm/ header kısmının %40'ı ve img görseli eklendi

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -4,6 +4,13 @@ function OrderForm() {
     return (
         <div>
 
+            <header className='orderForm-header'>
+
+                <h1 className='orderForm-header-h1'>Teknolojik Yemekler</h1>
+                <img className='orderForm-header-img' src='../images/iteration-2-images/pictures/form-banner.png' />
+
+            </header>
+
         </div>
     )
 }
